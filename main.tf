adc1284e58df574101ef24f43228b4cda4c34fed
Thomas Skill <tskill@us.ibm.com>
2021-10-18 16:29:35 -0500

    rdr

--- main.tf
+++ main.tf
@@ -15,7 +15,37 @@ module setup_clis {
 ##
 
 
+# cert-manager
+resource "null_resource" "deploy_cert-manager" {
+  triggers = {
+    kubeconfig = var.cluster_config_file
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/deployCertManager.sh"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${path.module}/scripts/deployCertManager.sh destroy"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+}
+
+
+# BAS
+
+# SLS
+
 # Service Binding Operator / Catalog
+/*
 resource "null_resource" "deploy_catalog" {
   triggers = {
     kubeconfig = var.cluster_config_file
@@ -39,36 +69,7 @@ resource "null_resource" "deploy_catalog" {
   }
 }
 
-
-# cert-manager
-/*
-resource "null_resource" "deploy_cert-manager" {
-  triggers = {
-    kubeconfig = var.cluster_config_file
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/deployCertManager.sh"
-
-    environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${path.module}/scripts/deployCertManager.sh destroy"
-
-    environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-    }
-  }
-}
 */
-
-# BAS
-
-# SLS
 
 
 
