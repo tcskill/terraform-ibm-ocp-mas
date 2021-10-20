28557114fdec4864047dd7e9674f4bb8fed28346
Thomas Skill <tskill@us.ibm.com>
2021-10-20 09:12:50 -0500

    basadd

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module setup_clis {
 # Setup Preq's before MAS85 core
 ##
 
-
+/*
 # cert-manager
 resource "null_resource" "deploy_cert-manager" {
   triggers = {
@@ -38,10 +38,37 @@ resource "null_resource" "deploy_cert-manager" {
     }
   }
 }
-
+*/
 
 # BAS
+resource "null_resource" "deploy_bas" {
+  triggers = {
 
+    basprojectname=var.bas_projectName
+    sc_archive=var.bas_storageClassArchive
+    sc_db=var.bas_storageClassDB
+    sc_kafka=var.bas_storageClassKafka
+    sc_zookeep=var.bas_storageClassZookeeper
+    kubeconfig = var.cluster_config_file
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/deployBAS.sh ${self.triggers.basprojectname} ${self.triggers.sc_archive} ${self.triggers.sc_db} ${self.triggers.sc_kafka} ${self.triggers.zookeep}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${path.module}/scripts/destroyBAS.sh ${self.triggers.basprojectname}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+    }
+  }
+}
 
 
 
