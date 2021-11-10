6a7f4d1abb230f726dc259bab4b3d4db119e606f
Thomas Skill <tskill@us.ibm.com>
2021-11-10 14:02:01 -0600

    commondestry

--- scripts/deployCommon.sh
+++ scripts/deployCommon.sh
@@ -2,10 +2,17 @@
 
 if [[ "$1" == "destroy" ]]; then
     echo "removing common services ..."
-    #kubectl delete ClusterServiceVersion service-binding-operator.v0.8.0 -n openshift-operators
     kubectl delete OperatorGroup operatorgroup -n ibm-common-services
     kubectl delete ClusterRoleBinding ibm-common-service-webhook-ibm-common-services
     kubectl delete subscription ibm-common-service-operator -n ibm-common-services
+    kubectl delete ClusterRole commonservices.operator.ibm.com-v3-admin
+    kubectl delete ClusterRole commonservices.operator.ibm.com-v3-crdview
+    kubectl delete ClusterRole commonservices.operator.ibm.com-v3-edit
+    kubectl delete ClusterRole commonservices.operator.ibm.com-v3-view
+    kubectl delete ClusterRole ibm-common-service-webhook
+    kubectl delete ClusterRoleBinding secretshare-ibm-common-services
+    kubectl delete ClusterRoleBinding system:openshift:controller:image-import-controller
+
     kubectl delete namespace ibm-common-services
 
 else 
